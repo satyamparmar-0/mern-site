e553d627fbd98fac5dbe8f8e6da2b2e54b8e720b
satyam <satyamparmarsatyam@gmail.com>
2024-03-26 21:42:32 +0530

    commit

---
+++ Controllers/User.js
@@ -0,0 +1,38 @@
+const Student = require("../Models/User");
+
+// here are all the logic of the app for the student
+
+exports.AddStudent = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = new Student({
+      username: username,
+      password: password,
+    });
+
+    await user.save();
+    res.send({ status: true, message: "user created" });
+  } catch (error) {
+    console.log("Error while Signing Up", error.message);
+    res.status(500).send("internal server error");
+  }
+};
+
+exports.Login = async(req,res)=>{
+    try{
+        const {username,password} = req.body;
+        const user = await Student.findOne({username});
+        if(!user){
+            res.send("Username not found or user not registered");
+        }
+        if(password!=user.password){
+            res.send("Incorrect password");
+        }
+    }
+    catch(error){
+            console.log('Erro while Login',error.message);
+            res.status(500).send('Internal server error');
+    }
+}
+
+
